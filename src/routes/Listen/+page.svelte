abc2982fa9f28eae7b3b2cce2a16ea6fa835946c
Charlie Webber <charlie@purefinance.com.au>
2023-07-25 17:06:36 +1000

    seo commit

--- src/routes/Listen/+page.svelte
+++ src/routes/Listen/+page.svelte
@@ -17,3 +17,7 @@ class="mx-auto md:w-1/2 lg-md:w-1/4 w-3/4 mt-6"
 	src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/702705951&color=%23bb5404&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
 />
 
+<svelte:head>
+	<title>Listen</title>
+	<meta name="description" content="Listen to Lulu's music." />
+</svelte:head>
