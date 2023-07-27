317218c0621bb73a2dcde02a3b88289c48c5a844
Charlie Webber <charlie@purefinance.com.au>
2023-07-27 11:24:29 +1000

    img sizes

--- src/components/Navbar/HamburgerNav/Logo.svelte
+++ src/components/Navbar/HamburgerNav/Logo.svelte
@@ -5,7 +5,7 @@ import { page } from "$app/stores";
 
 <p class="font-display text-white my-auto">Menu</p>
 <div class="flex rounded-full ml-auto max-w-24 max-h-24">
-    <a href="/"><img src="./images/profilepicture.webp" alt="Lulu Profile" class=" {($page.url.pathname === '/') ? 'selected' : ''} flex rounded-full ml-auto max-w-24 max-h-24 border-b-[1px] border-transparent"/></a>
+    <a href="/"><img src="./images/profilepicture.webp" alt="Lulu Profile" width="100" class=" {($page.url.pathname === '/') ? 'selected' : ''} flex rounded-full ml-auto max-w-24 max-h-24 border-b-[1px] border-transparent"/></a>
 </div>
 <style>
     img {
